22bc6f75ca77056f28c9b2c6895cf5d767873cab
solonkonora <nkwadanora@gmail.com>
2025-04-19 19:10:13 +0100

    formatted code of the types folder

--- nav-types/types.ts
+++ nav-types/types.ts
@@ -1,12 +1,10 @@
-// src/navigation/types.ts
 export type todoProps = {
-    _id: string;
-    title: string;
-    description: string;
-  };
-  
-  export type RootStackParamList = {
-    Home: undefined;
-    'Add Task': { todo?: todoProps };
-  };
-  
+  _id: string;
+  title: string;
+  description: string;
+};
+
+export type RootStackParamList = {
+  Home: undefined;
+  'Add Task': { todo?: todoProps };
+};
